99234b3ae2ef795e9efcc35693ae3b41b70e79f5
Sapna Fulwani <imsapnaf13@gmail.com>
2024-06-05 11:34:06 +0530

    Initial Commit

---
+++ MatchMate/MatchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by Harish Fulwani on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
